1df3453278f8feb5df6a446688cdb1863f89de2e
Kevin Rode <krode17@gmail.com>
2020-11-08 13:07:00 -0500

    Week 10 Homework

---
+++ Week10/homework/ThreatIntel.ps1
@@ -0,0 +1,21 @@
+﻿$optionArray = @('All', 'Stopped', 'Running', 'q')
+$option = Read-Host -Prompt 'Would you like to view [All] [Running] or [Stopped] services? Press [q] to quit'
+if ( -not ($optionArray -contains $option))  
+    {
+        Write-Output "Option not found. Please specify 'All' 'Running' or 'Stopped'"
+    }
+if ($option -eq 'q')
+    {
+        exit
+    }
+else 
+    {
+        if ( $option -eq "All" )
+            {
+                Get-Service
+            }
+        else
+            {
+                Get-Service | where {$_.Status -eq $option}
+            }
+    }
